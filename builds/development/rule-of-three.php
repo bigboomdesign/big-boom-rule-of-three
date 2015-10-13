17c9a3a9e2c6259618a5cd32bed63ffcbfce1ee2
Michael Hull <michael@resoundingechoes.net>
2015-10-13 12:17:09 -0400

    Changed plugin name

--- builds/development/rule-of-three.php
+++ builds/development/rule-of-three.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Rule Of Three
+ * Plugin Name: Big Boom Rule Of Three
  * Description: Uses shortcode to insert a responsive, custom-defined rule of 3 into a page or post
- * Version: 0.5.1
+ * Version: 0.5.2
  * Author: Big Boom Design
  * Author URI: http://bigboomdesign.com
  */
